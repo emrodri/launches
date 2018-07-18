16a97f5a15df1afa140b5a7c6156baea16b9c976
Emilio Rodriguez <emilio.rodriguez@azulae.es>
2018-07-18 13:46:14 +0200

    change detection strategies

--- src/app/launch/launch-bar/launch-bar.component.ts
+++ src/app/launch/launch-bar/launch-bar.component.ts
@@ -1,10 +1,12 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
 import {Launch} from '../../store/models/launch.model';
 
 @Component({
   selector: 'app-launch-bar',
   templateUrl: './launch-bar.component.html',
-  styleUrls: ['./launch-bar.component.css']
+  styleUrls: ['./launch-bar.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+
 })
 export class LaunchBarComponent implements OnInit {
   @Input('launch') launch: Launch;
